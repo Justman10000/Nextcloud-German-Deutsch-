e313ad6114fb2048cf557e6ab4d1246194a2fb0e
Justman10000 <49990629+Justman10000@users.noreply.github.com>
2021-05-10 13:47:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
+echo "Installiere git..."
 apt install git -y
+echo "Klone Repositorie..."
 git clone https://github.com/Justman10000/Nextcloud-German-Deutsch-.git .
+echo "Entferne .git Ordner..."
 rm -r .git
+echo "Entferne README..."
 rm -r README.md
+echo "Benenne PHP Datei um..."
 mv setup-nextcloud.php index.php
+echo "Setze Rechte..."
 chmod -R 777 *
